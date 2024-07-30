b28b0a1729987a7caeac850d94252c75f3783af4
Kodah <djbeast35@gmail.com>
2024-07-30 15:57:51 -0500

    .

---
+++ txData/ESXLegacy/resources/[dev]/scully_mba/client/main.lua
@@ -0,0 +1,46 @@
+local hasSpawned = false
+
+local function setMBA(entitySet)
+    local interior = GetInteriorAtCoords(-324.22, -1968.49, 20.60)
+
+    if interior ~= 0 then
+        local removeSets, newEntitySet = Config.Removals.interiors, Config.Sets[entitySet]
+        local removeSigns, newSign = Config.Removals.signs, Config.Signs[entitySet]
+
+        for i = 1, #removeSets do
+            DeactivateInteriorEntitySet(interior, removeSets[i])
+        end
+
+        for i = 1, #removeSigns do
+            RemoveIpl(removeSigns[i])
+        end
+
+        Wait(100)
+
+        for i = 1, #newEntitySet do
+            ActivateInteriorEntitySet(interior, newEntitySet[i])
+        end
+
+        if newSign then
+            RequestIpl(newSign)
+        end
+
+        RefreshInterior(interior)
+    end
+end
+
+CreateThread(function()
+    TriggerEvent('chat:addSuggestion', '/setmba', 'Set the maze bank arena interior.', {{ name = 'interior', help = 'The interior entity set name' }})
+end)
+
+AddEventHandler('playerSpawned', function()
+    if not hasSpawned then
+        setMBA(GlobalState.mba)
+
+        hasSpawned = true
+    end
+end)
+
+AddStateBagChangeHandler('mba', nil, function(bagName, key, value, _unused, replicated)
+    setMBA(value)
+end)
