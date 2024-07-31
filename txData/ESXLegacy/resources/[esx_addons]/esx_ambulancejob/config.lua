ab7b67ff6f97bf3a401f7f79df97089fef77c82e
Kodah <djbeast35@gmail.com>
2024-07-30 23:18:52 -0500

    +  MLOs

--- txData/ESXLegacy/resources/[esx_addons]/esx_ambulancejob/config.lua
+++ txData/ESXLegacy/resources/[esx_addons]/esx_ambulancejob/config.lua
@@ -1,12 +1,12 @@
 Config                            = {}
 
-Config.DrawDistance               = 10.0 -- How close do you need to be in order for the markers to be drawn (in GTA units).
+Config.DrawDistance               = 20.0 -- How close do you need to be in order for the markers to be drawn (in GTA units).
 Config.Debug                      = ESX.GetConfig().EnableDebug
 Config.Marker                     = {type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false}
 
 Config.ReviveReward               = 700  -- Revive reward, set to 0 if you don't want it enabled
 Config.SaveDeathStatus              = true -- Save Death Status?
-Config.LoadIpl                    = true -- Disable if you're using fivem-ipl or other IPL loaders
+Config.LoadIpl                    = false -- Disable if you're using fivem-ipl or other IPL loaders
 
 Config.Locale = GetConvar('esx:locale', 'en')
 
@@ -27,9 +27,9 @@ Config.BleedoutTimer              = 60000 * 10 -- time til the player bleeds out
 
 Config.EnablePlayerManagement     = false -- Enable society managing (If you are using esx_society).
 
-Config.RemoveWeaponsAfterRPDeath  = true
-Config.RemoveCashAfterRPDeath     = true
-Config.RemoveItemsAfterRPDeath    = true
+Config.RemoveWeaponsAfterRPDeath  = false
+Config.RemoveCashAfterRPDeath     = false
+Config.RemoveItemsAfterRPDeath    = false
 
 -- Let the player pay for respawning early, only if he can afford it.
 Config.EarlyRespawnFine           = false
